6402b770fdbc74b6095662ffa2f0947655b24c41
Felix Ruess <felix.ruess@gmail.com>
2014-09-09 18:49:26 +0200

    [airborne] send statistics for ahrs/gyro dt in STATE_FILTER_STATUS

--- sw/airborne/firmwares/fixedwing/main_ap.c
+++ sw/airborne/firmwares/fixedwing/main_ap.c
@@ -135,14 +135,28 @@ static inline void on_gyro_event( void );
 static inline void on_accel_event( void );
 static inline void on_mag_event( void );
 volatile uint8_t ahrs_timeout_counter = 0;
+// variables for statistics on dt
+static float dt_avg = 0.0;
+static float dt_sum = 0.0;
+static float dt_min = FLT_MAX;
+static float dt_max = FLT_MIN;
+static uint32_t dt_cnt = 0;
 
 //FIXME not the correct place
-static void send_fliter_status(void) {
+static void send_filter_status(void) {
   uint8_t mde = 3;
   if (ahrs.status == AHRS_UNINIT) mde = 2;
   if (ahrs_timeout_counter > 10) mde = 5;
   uint16_t val = 0;
-  DOWNLINK_SEND_STATE_FILTER_STATUS(DefaultChannel, DefaultDevice, &mde, &val);
+  if (dt_cnt > 0)
+    dt_avg = dt_sum / dt_cnt;
+  DOWNLINK_SEND_STATE_FILTER_STATUS(DefaultChannel, DefaultDevice, &mde, &val,
+                                    &dt_avg, &dt_min, &dt_max);
+  // reset dt statistics
+  dt_sum = 0.0;
+  dt_cnt = 0;
+  dt_min = FLT_MAX;
+  dt_max = FLT_MIN;
 }
 
 #endif // USE_AHRS && USE_IMU
@@ -197,7 +211,7 @@ void init_ap( void ) {
 #endif
 
 #if USE_AHRS && USE_IMU
-  register_periodic_telemetry(DefaultPeriodic, "STATE_FILTER_STATUS", send_fliter_status);
+  register_periodic_telemetry(DefaultPeriodic, "STATE_FILTER_STATUS", send_filter_status);
 #endif
 
   air_data_init();
@@ -741,10 +755,21 @@ PRINT_CONFIG_MSG("Calculating dt for AHRS/INS propagation.")
   // dt between this and last callback
   float dt = now_ts - last_ts;
   last_ts = now_ts;
+
+  // get some statistics on dt
+  dt_sum += dt;
+  dt_cnt++;
+  if (dt < dt_min)
+    dt_min = dt;
+  if (dt > dt_max)
+    dt_max = dt;
 #else
 PRINT_CONFIG_MSG("Using fixed AHRS_PROPAGATE_FREQUENCY for AHRS/INS propagation.")
 PRINT_CONFIG_VAR(AHRS_PROPAGATE_FREQUENCY)
   const float dt = (1./AHRS_PROPAGATE_FREQUENCY);
+  dt_avg = dt;
+  dt_min = dt;
+  dt_max = dt;
 #endif
 
   ahrs_timeout_counter = 0;
